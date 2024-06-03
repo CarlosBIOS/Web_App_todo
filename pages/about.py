c13913bed912eee15504097f9c5631da11b6f35a
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-03 11:58:51 +0100

    Agora posso retirar item from todo_done

--- pages/about.py
+++ pages/about.py
@@ -5,3 +5,8 @@ todos_complete = get_and_write_todos(filepath='pages/todo_done.txt')
 
 for index, todo in enumerate(todos_complete):
     checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        get_and_write_todos(remove_item=todo, filepath='pages/todo_done.txt')
+        del todos_complete[index]
+        del st.session_state[todo]
+        st.rerun()
